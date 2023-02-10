32c379fb58ccbd5e1163575d4484ed768a9aa7ef
n2tuan <83774263+n2tuan@users.noreply.github.com>
2023-02-10 11:31:33 +0700

    Update IT_CERT_Python_Sending_Email.py

--- IT_CERT_Python_Sending_Email.py
+++ IT_CERT_Python_Sending_Email.py
@@ -48,3 +48,30 @@
       format_car(max_sale["car"]), max_sale["sale"]),
   ]
 
+    
+    
+from email.message import EmailMessage
+import mimetypes
+import smtplib
+import os
+
+def generate_email(sender, recipient, subject, body, attachment=None):
+  message = EmailMessage()
+  message['From'] = sender
+  message['To'] = recipient
+  message['Subject'] = subject
+  message.set_content(body)
+
+  if attachment:
+    mime_type, _ = mimetypes.guess_type(attachment)
+    mime_type, mime_subtype = mime_type.split('/', 1)
+
+    with open(attachment, 'rb') as ap:
+      message.add_attachment(ap.read(),maintype=mime_type,subtype=mime_subtype,filename=os.path.basename(attachment))
+
+  return message
+
+def send_email(message):
+  mail_server = smtplib.SMTP('localhost')
+  mail_server.send_message(message)
+  mail_server.quit()
